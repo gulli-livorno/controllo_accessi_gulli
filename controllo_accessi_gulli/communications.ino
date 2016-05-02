eef3f7c6d8dda82adc66e4f446a1aabc6bfd4fad
subakun <stefano.baldacci@gmail.com>
2016-05-02 11:37:08 +0200

    baseline

---
+++ controllo_accessi_gulli/communications.ino
@@ -0,0 +1,59 @@
+
+/*
+ *********************Arduino Source File Header**************************
+__file_name__ = "communications.ino"
+__description__="cosa fa questo codice ?"
+__author__ = ""
+__copyright__ = "Informazioni di Copyright"
+__license__ = "GPL"
+__email__ = " "
+__status__ = "Development[X]";"Test[]";"Production[]";
+__History__: (repeat the following line as many times as applicable)
+    __version__ = "0.1 original"
+***************************************************************************
+*/
+
+
+
+//TODO
+/* implementare un modulo per la comunicazione con un PC connesso via seriale
+o via Ethernet.
+
+Il SW controllo accessi che gira su Arduino dovrebbe:
+
+1) comunicare al PC connesso
+quando c'è stato un passaggio attraverso la barriera in modo che il PC connesso possa
+aggiornare un display.
+
+2) comunicare al PC connesso
+se una barriera è bloccata in modo da generare un allarme audio/video
+
+3) altre cose che ci vengono in mente . . . . .
+*/
+
+
+int TxCounters(){
+
+return 0;
+}
+
+int TxIngresso(){
+
+return 0;
+}
+
+int TxUscita(){
+
+return 0;
+}
+
+int TxBLocked(){
+
+return 0;
+}
+
+int ProcessSerialInput(){
+
+return 0;
+}
+
