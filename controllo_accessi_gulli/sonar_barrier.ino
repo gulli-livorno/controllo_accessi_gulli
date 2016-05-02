a1c9fe868660910b50fa3b106ecd60b4eea9c7b8
subakun <stefano.baldacci@gmail.com>
2016-05-02 11:35:23 +0200

    baseline

---
+++ controllo_accessi_gulli/sonar_barrier.ino
@@ -0,0 +1,31 @@
+
+/*
+ *********************Arduino Source File Header**************************
+__file_name__ = "sonar_barrier.ino"
+__description__="cosa fa questo codice ?"
+__author__ = "Stefano Baldacci"
+__copyright__ = "Informazioni di Copyright"
+__license__ = "GPL"
+__email__ = "stefano.baldacci@gmail.com"
+__status__ = "Development[X]";"Test[]";"Production[]";
+__History__: (repeat the following line as many times as applicable)
+    __version__ = "0.1 original"
+***************************************************************************
+*/
+
+
+
+bool IsBarrierCrossed(NewPing sonar) {
+
+//TODO: vedi specifiche in .docs/IsBarrierCrossed.pdf
+return false;
+
+  }
+
+  int ReadSonars(){
+
+//TODO: vedi specifiche in .docs/ReadSonar.pdf
+
+return 0;
+
+  }
