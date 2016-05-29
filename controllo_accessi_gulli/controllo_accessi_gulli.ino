cf649de2dd4662716c8f7798de639755a7bf5ae4
Stefano Baldacci <stefano.baldacci@gmail.com>
2016-05-30 00:50:05 +0200

    aggiunti commenti al codice SM

--- controllo_accessi_gulli/controllo_accessi_gulli.ino
+++ controllo_accessi_gulli/controllo_accessi_gulli.ino
@@ -31,11 +31,10 @@ __History__: (repeat the following line as many times as applicable)
 #define SM_STATUS_PI 2
 #define SM_STATUS_PU 3
 
+// tempo in [msec] per attraversare la barriera
+#define RITARDO 5000
 
-#define RITARDO 5000 // tempo in [msec] per attraversare la barriera
-
-
-//codifica dello stato delle barriere
+//codifica dello stato delle barriere sonar
 #define SB_EXT_X B00000010 //barriera esterna attraversata
 #define SB_INT_X B00000001 //barriera interna attraversata
 #define SB_NONE  B00000000 // barriere libere
@@ -45,7 +44,6 @@ __History__: (repeat the following line as many times as applicable)
 #define LED_SB_EXT 8
 
 //****************** global variables definition ******************
-long int aTime,bTime=0;
 int contaIngressi=0; // contatore degli avventuti ingressi
 int contaUscite=0;  // contatore delle avvenute uscite
 byte sonarBarries=B00000000; // così composta -> [0 0 0 0 0 0 extSonar intSonar ]
@@ -65,36 +63,32 @@ Serial.println(F("************************************"));
 Serial.println(F("GULLI: Controllo Accessi 1.0 Test SW"));
 Serial.println(F("************************************"));
 delay(5000);
-
 }
+
+
 
 void loop()
 {
-// TODO
 sonarBarries=_t_ReadSonars();
 Serial.print("\rRead Sonars: ");
 Serial.print(sonarBarries, BIN);
-//Serial.print(" loop time[us]= ");
-/*bTime=aTime;
-aTime=micros();
-Serial.print("lt=");
-Serial.println(aTime-bTime);
-*/
+
+// ************************** STATE MACHINE *******************************//
 switch (SM_STATUS) {
   case SM_STATUS_L: // stato libero
   Serial.print(F(" ->SM_STATUS_L       "));
     TTV_timer=millis(); //inizializzo Timer attraversamento barriere
-    switch (sonarBarries) {
-      case SB_EXT_X:
-        SM_STATUS=SM_STATUS_PI;
+    switch (sonarBarries) { // controllo lo stato delle barriere sonar
+      case SB_EXT_X: // rilevato attraversamento barriera esterna
+        SM_STATUS=SM_STATUS_PI; // mando la State Machine nello stato di Pre-Ingresso
         Serial.println(F("\tSB_EXT_X"));
       break;
-      case SB_INT_X:
-        SM_STATUS=SM_STATUS_PU;
+      case SB_INT_X: // rilevato attraversamento barriera interna
+        SM_STATUS=SM_STATUS_PU; // mando la State Machine nello stato di Pre-Uscita
           Serial.println(F("\tSB_INT_X"));
       break;
-      case SB_NONE:
-        SM_STATUS=SM_STATUS_L;
+      case SB_NONE: // nessun attraversamento barriera rilevato
+        SM_STATUS=SM_STATUS_L; // rimango nello stato Libero
       break;
 
       default:
@@ -106,28 +100,24 @@ switch (SM_STATUS) {
   case SM_STATUS_PI: // stato di pre-ingresso
     Serial.print(F(" ->SM_STATUS_PI       "));
     switch (sonarBarries) {
-      case SB_EXT_X: // nessun ingresso. Il soggetto è tornato indietro ....
-        SM_STATUS=SM_STATUS_L;
+      case SB_EXT_X: // rilevato attraversamento barriera esterna. Il soggetto è tornato indietro
+        SM_STATUS=SM_STATUS_L; // mando la State Machine nello stato di Libero
         Serial.println(F("SB_EXT_X - Mancato ingresso !"));
       break;
-      case SB_INT_X:
-        //è avvenuto un ingresso
-        contaIngressi++;
-        SM_STATUS=SM_STATUS_L;
+      case SB_INT_X: // rilevato attraversamento barriera esterna. E' avvenuto un ingresso
+        contaIngressi++; // incremento contatore ingressi
+        SM_STATUS=SM_STATUS_L; // torno nello stato Libero
         Serial.println(F("SB_INT_X - Ingresso-->[]"));
         TxIngresso();
       break;
-      case SB_NONE: // in attesa di un attraversamento . .
-      //Serial.println(TTV_timer-millis());
-      //delay(1000);
-        if ((millis()-TTV_timer)>=RITARDO) {
-          //il timer attraversamento barriera è scaduto
-          //torno nello stato libero
+      case SB_NONE: // nessun attraversamento barriera rilevato. In attesa di un attraversamento
+        if ((millis()-TTV_timer)>=RITARDO) { // controllo il timer di attraversamento barriera
+          //il timer attraversamento barriera è scaduto. Torno nello stato libero
           Serial.println(F("SB_NONE - Timeout scaduto - NO ingresso ! "));
           SM_STATUS=SM_STATUS_L;
         } else {
-          // aspetto che il timer scada o ci sia un attraversamento della barriera INTerna
-          SM_STATUS=SM_STATUS_PI;
+          // aspetto che il timer scada o ci sia un attraversamento della barriera interna
+          SM_STATUS=SM_STATUS_PI; // rimango nello stato di Pre-ingresso fino a che il timer non scade
           Serial.println(F("SB_NONE - In attesa di ingresso "));
         }
       break;
@@ -139,24 +129,23 @@ switch (SM_STATUS) {
   case SM_STATUS_PU: // stato di pre-uscita
     Serial.print(F(" ->SM_STATUS_PU       "));
     switch (sonarBarries) {
-      case SB_EXT_X: // è avvenuta una uscita
-        contaUscite++;
+      case SB_EXT_X: // rilevato attraversamento barriera esterna. E' avvenuta una uscita
+        contaUscite++; // incremento contatore uscite
         Serial.println(F("SB_EXT_X - Uscita[]-->"));
-        SM_STATUS=SM_STATUS_L;
+        SM_STATUS=SM_STATUS_L; // torno nello stato Libero
       break;
-      case SB_INT_X: // nessun ingresso. Il soggetto è tornato indietro ...
-        SM_STATUS=SM_STATUS_L;
+      case SB_INT_X: // // rilevato attraversamento barriera interna. Il soggetto è tornato indietro
+        SM_STATUS=SM_STATUS_L; // torno nello stato Libero
         Serial.println(F("SB_EXT_X - Mancata uscita !"));
       break;
-      case SB_NONE: // in attesa di un attraversamento . . .
+      case SB_NONE: // // nessun attraversamento barriera rilevato. In attesa di un attraversamento
           if ((millis()-TTV_timer)>=RITARDO) {
-          //il timer attraversamento barriera è scaduto
-          //torno nello stato libero
+          //il timer attraversamento barriera è scaduto torno nello stato libero
           Serial.println(F("SB_NONE - Timeout scaduto - NO uscita ! "));
           SM_STATUS=SM_STATUS_L;
         } else {
-          // aspetto che il timer scada o ci sia un attraversamento della barriera EXTerna
-          SM_STATUS=SM_STATUS_PU;
+          // // aspetto che il timer scada o ci sia un attraversamento della barriera esterna
+          SM_STATUS=SM_STATUS_PU; // rimango nello stato di Pre-uscita fino a che il timer non scade
           Serial.println(F("SB_NONE - In attesa di uscita "));
         }
       break;
@@ -169,7 +158,9 @@ switch (SM_STATUS) {
   break;
 
 }
-// State machine
+// ************************** STATE MACHINE *******************************//
+
+// TODO
 // Data display and communication
 
 }
