b4a103594eefddd5de7419c6c39817a7898e7d01
Stefano Baldacci <stefano.baldacci@gmail.com>
2016-08-03 23:38:22 +0200

    master allineato con test_sonar+test_comm

--- controllo_accessi_gulli/controllo_accessi_gulli.ino
+++ controllo_accessi_gulli/controllo_accessi_gulli.ino
@@ -1,9 +1,8 @@
 /*
  *********************Arduino Source File Header**************************
 __file_name__ = controllo_accessi_gulli.ino
-__description__="Gestisce la state machine oper il controllo accessi  attraverso
- le barriere sonar e si connette con un PC esterno (via seriale o Ethernet) per
- comunicare gli aggiornameti degli accessi o condizioni anomale di blocco delle barriere"
+__description__="Modulo per la prova delle funzionalità dei sensori sonar
+e la liberria NewPing "
 __author__ = "Stefano Baldacci"
 __copyright__ = "Informazioni di Copyright"
 __license__ = "GPL"
@@ -16,7 +15,7 @@ __History__: (repeat the following line as many times as applicable)
 */
 
 //#include "Arduino.h" // libreria per compatibilita con PlatformIO. Vedi www.platformio.org
-#include <NewPing.h> // librearia per gestione moduli Sonar HC-SR04
+#include <NewPing.h> // libreria per gestione moduli Sonar HC-SR04
 
 //****************** global variables definition ******************
 // Collegamenti moduli Sonar
@@ -24,15 +23,9 @@ __History__: (repeat the following line as many times as applicable)
 #define EXT_SONAR_ECHO_PIN  4
 #define INT_SONAR_TRIG_PIN 5
 #define INT_SONAR_ECHO_PIN 6
-#define MAX_SONAR_DISTANCE 100 //[cm]
+#define LARGHEZZA_VARCO 100  //[cm]
+#define SPEED_OF_SOUND 33.0f    //[cm/ms]
 
-// codifica stati della State Machine
-#define SM_STATUS_L 1
-#define SM_STATUS_PI 2
-#define SM_STATUS_PU 3
-
-// tempo in [msec] per attraversare la barriera
-#define RITARDO 5000
 
 //codifica dello stato delle barriere sonar
 #define SB_EXT_X B00000010 //barriera esterna attraversata
@@ -44,123 +37,126 @@ __History__: (repeat the following line as many times as applicable)
 #define LED_SB_EXT 8
 
 //****************** global variables definition ******************
-int contaIngressi=0; // contatore degli avventuti ingressi
-int contaUscite=0;  // contatore delle avvenute uscite
 byte sonarBarries=B00000000; // così composta -> [0 0 0 0 0 0 extSonar intSonar ]
-byte SM_STATUS = SM_STATUS_L;  //stato iniziale  della SM
+
 unsigned long TTV_timer=0; // timer per attraversamento barriera
+#define TTV_DELAY 1500 //tempo attraversamento barriera [ms]
+bool timeout=false;
+
+int contaIngressi=0;
+int contaUscite=0;
+
+
+int i=0;
+float dist=0.0;
 
 // oggetti NewPing per la gestione dei moduli sonar
-NewPing ext_Sonar(EXT_SONAR_TRIG_PIN,EXT_SONAR_ECHO_PIN,MAX_SONAR_DISTANCE);
-NewPing int_Sonar(INT_SONAR_TRIG_PIN,INT_SONAR_ECHO_PIN,MAX_SONAR_DISTANCE);
+NewPing ext_Sonar(EXT_SONAR_TRIG_PIN,EXT_SONAR_ECHO_PIN,LARGHEZZA_VARCO);
+NewPing int_Sonar(INT_SONAR_TRIG_PIN,INT_SONAR_ECHO_PIN,LARGHEZZA_VARCO);
 
-
+String msg="";
 
 void setup()
 {
 Serial.begin(115200);
-Serial.println(F("************************************"));
-Serial.println(F("GULLI: Controllo Accessi 1.0 Test SW"));
-Serial.println(F("************************************"));
-delay(5000);
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+tone(10, 200, 500);
 }
-
-
 
 void loop()
 {
-sonarBarries=_t_ReadSonars();
-Serial.print("\rRead Sonars: ");
-Serial.print(sonarBarries, BIN);
 
-// ************************** STATE MACHINE *******************************//
-switch (SM_STATUS) {
-  case SM_STATUS_L: // stato libero
-  Serial.print(F(" ->SM_STATUS_L       "));
-    TTV_timer=millis(); //inizializzo Timer attraversamento barriere
-    switch (sonarBarries) { // controllo lo stato delle barriere sonar
-      case SB_EXT_X: // rilevato attraversamento barriera esterna
-        SM_STATUS=SM_STATUS_PI; // mando la State Machine nello stato di Pre-Ingresso
-        Serial.println(F("\tSB_EXT_X"));
-      break;
-      case SB_INT_X: // rilevato attraversamento barriera interna
-        SM_STATUS=SM_STATUS_PU; // mando la State Machine nello stato di Pre-Uscita
-          Serial.println(F("\tSB_INT_X"));
-      break;
-      case SB_NONE: // nessun attraversamento barriera rilevato
-        SM_STATUS=SM_STATUS_L; // rimango nello stato Libero
-      break;
+  /*
+  bool BX=false;
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  Serial.print(F("Ping Ext Sonar:"));
+  Serial.print(ext_Sonar.ping_cm());
+  Serial.print(F("  Ping Int Sonar:"));
+  Serial.print(int_Sonar.ping_cm());
+  Serial.print(F("    Ingressi="));
+  Serial.print(contaIngressi);
+  Serial.print(F("  Uscite="));
+  Serial.print(contaUscite);
+  Serial.print("\r");
 
-      default:
-        SM_STATUS=SM_STATUS_L;
-      break;
+  //Controllo INGRESSO
+  if (IsBarrierCrossed(ext_Sonar,8,LARGHEZZA_VARCO*0.9,0.6)) {
+    TTV_timer=millis(); //inizializzo timer
+    delay(6);
+    while (! (IsBarrierCrossed(int_Sonar,5,LARGHEZZA_VARCO*0.9,0.6)||timeout)) { // attendo attraversamento barriera interna o timeout
+      if (millis()-TTV_timer>TTV_DELAY) {
+        timeout=true;
+      }
     }
-  break;
+      if (! timeout) { // se sono uscito dal loop NON per timeout allora è un ingresso
+        contaIngressi++;
+        digitalWrite(8,HIGH);
+        tone(11, 500, 500);
 
-  case SM_STATUS_PI: // stato di pre-ingresso
-    Serial.print(F(" ->SM_STATUS_PI       "));
-    switch (sonarBarries) {
-      case SB_EXT_X: // rilevato attraversamento barriera esterna. Il soggetto è tornato indietro
-        SM_STATUS=SM_STATUS_L; // mando la State Machine nello stato di Libero
-        Serial.println(F("SB_EXT_X - Mancato ingresso !"));
-      break;
-      case SB_INT_X: // rilevato attraversamento barriera esterna. E' avvenuto un ingresso
-        contaIngressi++; // incremento contatore ingressi
-        SM_STATUS=SM_STATUS_L; // torno nello stato Libero
-        Serial.println(F("SB_INT_X - Ingresso-->[]"));
-        TxIngresso();
-      break;
-      case SB_NONE: // nessun attraversamento barriera rilevato. In attesa di un attraversamento
-        if ((millis()-TTV_timer)>=RITARDO) { // controllo il timer di attraversamento barriera
-          //il timer attraversamento barriera è scaduto. Torno nello stato libero
-          Serial.println(F("SB_NONE - Timeout scaduto - NO ingresso ! "));
-          SM_STATUS=SM_STATUS_L;
-        } else {
-          // aspetto che il timer scada o ci sia un attraversamento della barriera interna
-          SM_STATUS=SM_STATUS_PI; // rimango nello stato di Pre-ingresso fino a che il timer non scade
-          Serial.println(F("SB_NONE - In attesa di ingresso "));
-        }
-      break;
-      default:
-        SM_STATUS=SM_STATUS_L;
-      break;
-    }
-  break;
-  case SM_STATUS_PU: // stato di pre-uscita
-    Serial.print(F(" ->SM_STATUS_PU       "));
-    switch (sonarBarries) {
-      case SB_EXT_X: // rilevato attraversamento barriera esterna. E' avvenuta una uscita
-        contaUscite++; // incremento contatore uscite
-        Serial.println(F("SB_EXT_X - Uscita[]-->"));
-        SM_STATUS=SM_STATUS_L; // torno nello stato Libero
-      break;
-      case SB_INT_X: // // rilevato attraversamento barriera interna. Il soggetto è tornato indietro
-        SM_STATUS=SM_STATUS_L; // torno nello stato Libero
-        Serial.println(F("SB_EXT_X - Mancata uscita !"));
-      break;
-      case SB_NONE: // // nessun attraversamento barriera rilevato. In attesa di un attraversamento
-          if ((millis()-TTV_timer)>=RITARDO) {
-          //il timer attraversamento barriera è scaduto torno nello stato libero
-          Serial.println(F("SB_NONE - Timeout scaduto - NO uscita ! "));
-          SM_STATUS=SM_STATUS_L;
-        } else {
-          // // aspetto che il timer scada o ci sia un attraversamento della barriera esterna
-          SM_STATUS=SM_STATUS_PU; // rimango nello stato di Pre-uscita fino a che il timer non scade
-          Serial.println(F("SB_NONE - In attesa di uscita "));
-        }
-      break;
-      default:
-        SM_STATUS=SM_STATUS_L;
-      break;
-    }
-  break;
-  //default:
-  break;
+        Serial.print(F("INGRESSO ->[]                                             "));
+      }
+  }
 
+
+
+// note di test
+//Distanza moduli sonar 30 cm.
+//Larghezza varco 85 cm
+// altezza sensori 75
+
+
+ //delay per esaurire echo sonar
+ //delay(10);
+  // Controllo USCITA
+  //Controllo INGRESSO
+
+
+  if (IsBarrierCrossed(int_Sonar,5,LARGHEZZA_VARCO*0.9,0.6)) {
+    TTV_timer=millis(); //inizializzo timer
+    delay(6);
+    while (! (IsBarrierCrossed(ext_Sonar,8,LARGHEZZA_VARCO*0.9,0.6)||timeout)) { // attendo attraversamento barriera externa o timeout
+      if (millis()-TTV_timer>TTV_DELAY) {
+        timeout=true;
+      }
+    }
+      if (! timeout) { // se sono uscito dal loop NON per timeout allora è una uscita
+        contaUscite++;
+          digitalWrite(7,HIGH);
+          tone(11, 200, 500);
+
+        Serial.print(F("USCITA ->[]                                             "));
+      }
+  }
+
+  timeout=false;
+  Serial.print("\r");
+*/
+
+  Serial.print(10);
+  Serial.print(" ");
+  Serial.print(20);
+  Serial.print("\n");
+  delay(10);
+    //tone(11, 200, 100);
+
+
+
+ /* ************************ TEST ***************************
+dist=NormalizedSonarReading(ext_Sonar, 8,MAX_SONAR_DISTANCE);
+
+//msg=TestSonarModule(ext_Sonar,millis());
+if (dist< 0.6) {
+  Serial.print(F("********************************* "));
+  Serial.print(dist);
+  Serial.println(F(" *********************************"));
 }
-// ************************** STATE MACHINE *******************************//
+else {
+Serial.println(dist);
+//delay(random(6));
+}
+  ************************ TEST ************************** */
 
-// TODO
-// Data display and communication
 
 }
--- controllo_accessi_gulli/sonar_barrier.ino
+++ controllo_accessi_gulli/sonar_barrier.ino
@@ -2,7 +2,7 @@
  *********************Arduino Source File Header**************************
 __file_name__ = "sonar_barrier.ino"
 __description__="cosa fa questo codice ?"
-__author__ = "Stefano Baldacci"
+__author__ = ""
 __copyright__ = "Informazioni di Copyright"
 __license__ = "GPL"
 __email__ = "stefano.baldacci@gmail.com"
@@ -13,106 +13,80 @@ __History__: (repeat the following line as many times as applicable)
 */
 
 //************************************Implementazione Funzioni Globali ****************************************//
+#include <NewPing.h>
 
-/* funzione int IsBarrierCrossed(NewPing sonar):
-Controlla se una barriera sonar è stata attraversata correttamente.
+/* float NormalizedSonarReading(NewPing sonar, byte num_samples,int ref_distance ):
+Calcola la media delle letture del sonar in rapporto ad una distanza di riferimento.
+
+Parametri:
+NewPing sonar = o0ggetto di tipo NewPing per la gestione del modulo sonar
+byte num_samples =................ numero di letture sulle quali effettuare la media
+int ref_distance = lettura di riferimento rispetto alla quale si normalizza la media delle letture
+Ritorno:
+float rapporto della media delle letture rispetto alla larghezza del varco (stima di barriera libera)
+      valori > 0.9 indicano barriera libera
+      valori < 0.5 indicano barriera interrotta
 */
-bool IsBarrierCrossed(NewPing sonar) {
+float NormalizedSonarReading(NewPing sonar, byte num_samples,int ref_distance ) {
 
-//TODO: vedi specifiche in .docs/IsBarrierCrossed.pdf
-return false;
+  float acc_dist=0.0f; //accumulatore delle letture di distanza
+  for (byte i=0; i < num_samples;i++) { //eseguo letture ripetute
+    acc_dist+=sonar.ping_cm(); // le sommo tutte
+    // inserisco un delay. Non sono sicuro che serva ?
+    delay(2);
   }
+  acc_dist=acc_dist/num_samples; //calcolo la media delle letture
+  return acc_dist/ref_distance; // normalizzo rispetto alla lettura di riferimento iniziale
+}
 
-  /* funzione bool IsEXTBarrierCrossed():
-  Controlla se la  barriera sonar Esterna è stata attraversata correttamente.
-  */
-  bool IsEXTBarrierCrossed() {
-  //TODO: vedi specifiche in .docs/IsBarrierCrossed.pdf
-  return false;
-    }
-
-
-    /* funzione bool IsINTBarrierCrossed():
-    Controlla se la  barriera sonar Interna è stata attraversata correttamente.
-    */
-    bool IsINTBarrierCrossed() {
-
-    //TODO: vedi specifiche in .docs/IsBarrierCrossed.pdf
-    return false;
-      }
-
-
-
-  /* versione di test della{} IsEXTBarrierCrossed(NewPing sonar):
-  La barriera Sonar è simulata da un tasto collegato tra il pin
-  EXT_SONAR_ECHO_PIN e massa. Il pin deve essere in modo PULLUP
-  */
-  bool _t_IsEXTBarrierCrossed() {
-  //TODO: vedi specifiche in .docs/IsBarrierCrossed.pdf
-  byte return_value=false;
-  pinMode(EXT_SONAR_ECHO_PIN,INPUT_PULLUP );
-  // controllo se il tast che simula la barriera è premuto
-  if (digitalRead(EXT_SONAR_ECHO_PIN)==LOW) { // tasto premuto
-
-    //mi blocco fino a che il tasto è premuto ed accendo un LED
-    while(digitalRead(EXT_SONAR_ECHO_PIN)==LOW) digitalWrite(LED_SB_EXT,HIGH);
-
-    //il tasto è rilasciato e mi sblocco. Questo simula barriera attraversata.
-    //Spengo il LED
-    digitalWrite(LED_SB_EXT,LOW);
-
-    return_value=true;
-    }
-
-  return return_value;
-
-    }
-
-    /* versione di test della IsINTBarrierCrossed(NewPing sonar):
-    La barriera Sonar è simulata da un tasto collegato tra il pin
-    INT_SONAR_ECHO_PIN e massa. Il pin deve essere in modo PULLUP
-    */
-    bool _t_IsINTBarrierCrossed() {
-    byte return_value=false;
-    pinMode(INT_SONAR_ECHO_PIN,INPUT_PULLUP );
-    // controllo se il tast che simula la barriera è premuto
-    if (digitalRead(INT_SONAR_ECHO_PIN)==LOW) { // tasto premuto
-
-      //mi blocco fino a che il tasto è premuto ed accendo un LED
-      while(digitalRead(INT_SONAR_ECHO_PIN)==LOW) digitalWrite(LED_SB_INT,HIGH);
-
-      //il tasto è rilasciato e mi sblocco. Questo simula barriera attraversata.
-      //Spengo il LED
-      digitalWrite(LED_SB_INT,LOW);
-
-      return_value=true;
-      }
-
-    return return_value;
-
-      }
-
-/* funzione  byte _t_ReadSonars():
-Versione di test della ReadSonars()
+/* bool IsBarrierCrossed(NewPing sonar,int num_samples,int ref_distance,float margin):
+Controlla se la  barriera sonar  è stata attraversata correttamente.
+Viene stimata la probabilità che la barriera sia realmente interrotta
+misurando ripetutamente la distanza del sonar rispetto alla larghezza della barriera.
+Di seguito la funzione entra in in loop ed attende che la barriera
+venga liberata segnalando un attraversamento valido. Fintanto che la barriera non viene
+liberata la funzionegenera un messaggio di "Barriera bloccata"
+Parametri:
+NewPing sonar = oggetto di tipo NewPing per la gestione del modulo sonar
+int num_samples = numero di letture su cui fare la media
+int ref_distance = lettura del sonar quando la barriera è libera (circa 0.9 * MAX_SONAR_DISTANCE)
+float margin = margine  al di sotto/sopra  della quale considero la barriera interrotta/libera
+Ritorno:
+bool Vero se la barriera è stata attraversata correttamente,
+Falso altrimenti
 */
- byte _t_ReadSonars(){
+bool IsBarrierCrossed(NewPing sonar,int num_samples,int ref_distance,float margin){
 
-   byte returnValue=B00000000;
-   bool INT_Sonar,EXTSonar=false;
+  bool barrier_crossed=false;
 
-   INT_Sonar=_t_IsINTBarrierCrossed();
-   EXTSonar=_t_IsEXTBarrierCrossed();
-   bitWrite(returnValue,1,EXTSonar);
-   bitWrite(returnValue,0, INT_Sonar);
+    if (NormalizedSonarReading(sonar, num_samples, ref_distance) < margin) { // stimo validità interruzione
+      //interruzione valida
 
-   return returnValue;
+      while (! barrier_crossed) { // attendo riapertura barriera
+        if (NormalizedSonarReading(sonar, num_samples, ref_distance) > margin) { // stimo validità barriera aperta
+          //barriera aperta
+          barrier_crossed=true;
+        } else {
+          //barriera bloccata
+          Serial.println("Barriera bloccata >||<");
+          }
+      }
+    }
+
+  return barrier_crossed;
 }
 
 
-byte ReadSonars(){
 
-//TODO: vedi specifiche in .docs/ReadSonar.pdf
+String TestSonarModule(NewPing sonar,unsigned long start_time) {
 
-return 0;
+String msg=String("TSM: d[cm] t[ms]");
+String distance=String("");
+distance+=sonar.ping_cm();
+String atime=String("");
+atime+=(millis()-start_time);
+msg.replace("d",distance);
+msg.replace("t",atime);
+return msg;
 
 }
